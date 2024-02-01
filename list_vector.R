0465e9a30d796023e49d40a9008fbce41109610c
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-01 20:31:48 +0530

    Create list_vector.R

---
+++ list_vector.R
@@ -0,0 +1,25 @@
+#list
+list1<-list(c("hello","hii","bye","bdya"),c(6,2),10L,3+2i+25)
+list
+#naming of list
+list<-list(c("rahul","pooja"),c(6,2),list("MBA","BA"))
+list
+
+names(list)<-c("student name","marks","courses")
+list
+a<-c(10L,20L,30L)
+a
+b<-c("hello ","hii")
+b
+d<-c(TRUE,FALSE)
+d
+e<-list(a,b,d)
+e
+
+a<-list(c("sakshi","siddhi"),c(6,10),c("maths","cr"))
+a
+names(a)<-c("Name","marks","subjects")
+
+a[["address"]]<-c("delhi","kashmir")
+a
+
