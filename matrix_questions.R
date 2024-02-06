0f95f376dc453472505ccf4ba6ec98f8bb40145f
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-06 20:20:14 +0530

    Create matrix_questions.R

---
+++ matrix_questions.R
@@ -0,0 +1,107 @@
+# name all rows and column
+# Access 3rd rows
+# access 5th, 6th column 
+# access 8th column
+# asccess 1,2,3,4,5 column
+# access the 3rd row
+# access the rd row
+# access the 3rd row
+# access the  1st column
+# access the  2nd column
+# access the  3rd column
+# access the  4th column
+# access element in the 3rd column and 3rd rows
+# accesss element in the 2nd row and 5th column
+# modify elemenmts by o greater than 10
+# modify emements by -2 greaterthan 5
+# add two more rows 
+# add three more column trnaspose the matrix
+# delete 3rd row
+# delete 1st column
+# create two matrrix and perform all arthimatic operation
+
+
+rownamas=c("r1","r2","r3","r4","r5")
+
+columnnamas=c("c1","c2","c3","c4","c5","c6")
+
+a<-matrix(c(1:30),nrow=5,ncol=6, byrow=TRUE,dimnames=list(rownamas,columnnamas))
+a
+
+a[3,]
+
+a[5,]
+
+a[6,]
+
+a[,8]
+
+a[,1]
+
+a[,2]
+
+a[,4]
+
+a[3,3]
+
+a[2,5]
+a[a>5]<-0
+a
+a[a>5]<-2
+a
+
+
+
+#rbind()=add rors in matrix
+#cbind()
+rbind(a,c(12,14,151,16,15))
+a
+
+))
+d
+a[2,3]<-20
+a
+a[d=10]<-0
+a
+a[d>10]<0
+a
+d[d<45]<-0
+a
+a
+#rbind()=add wors in matrix
+#cbind()
+#rbind(a(c(12,14,151,16,32)@)
+t(a)
+a
+# Create a matrix of 30 elelments
+
+# name all rows and column
+# Access 3rd rows
+# access 5th, 6th column 
+# access 8th column
+# asccess 1,2,3,4,5 column
+# access the 3rd row
+# access the rd row
+# access the 3rd row
+# access the  1st column
+# access the  2nd column
+# access the  3rd column
+# access the  4th column
+# access element in the 3rd column and 3rd rows
+# accesss element in the 2nd row and 5th column
+# modify elemenmts by o greater than 10
+# modify emements by -2 greaterthan 5
+# add two more rows 
+# add three more column trnaspose the matrix
+# delete 3rd row
+# delete 1st column
+# create two matrrix and perform all arthimatic operation
+t[a]
+a
+
+a<-matrix(c(1:30),nrow=5,ncol=6)
+a
+b<-matrix(c(30:60),nrow=5,ncol=6)
+b
+a+b
+
